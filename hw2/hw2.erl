80a798b852720abd827417ffba042dfa7099e614
Tristan Rice <rice@fn.lc>
2017-01-26 21:18:19 -0800

    hw2: base

---
+++ hw2/hw2.erl
@@ -0,0 +1,204 @@
+-module(hw2).
+
+-export([mean/2, vec_add/2, vec_sum/1, vec_mean/2, set_nth/3]).
+-export([bank_statement/4, sample_transactions/0]).
+-export([sliding_average/5]).
+-export([sliding_average_help/3, sliding_average_seq/3, avg_demo/0]).
+
+-import(hw1, [closest/2]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Q1: mean
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% mean(WTree, DataKey) -> Mean
+%   WTree is a worker-tree produces by wtree:create.
+%   DataKey is the name of a list of numbers that is distributed across the workers.
+%   Here's an example of how one could use this:
+%       W = wtree:create(8).  % create a tree of eight worker processes
+%       L0_1000 = lists:seq(0,1000), ok.  % create the list [0, 1, 2, ..., 1000].
+%       % divide L0_1000 into length(W) pieces and send each piece to a worker in W.
+%       % each worker associates its piece with the key 'data'.
+%       workers:update(W, data, misc:cut(L0_1000, W)).
+%       Mean = hw2:mean(W, data).
+%   The last command should set Mean to 500.0
+mean(WTree, DataKey) ->
+  wtree:reduce(WTree,
+               fun(State) ->
+                   Nums = wtree:get(State, DataKey),
+                  lists:sum(Nums) / length(Nums) end,
+               fun(Left, Right) -> (Left+Right)/2 end).
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                          %
+% Q2: vec_mean                                                             %
+%                                                                          %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% vec_mean(WTree, DataKey) -> Mean
+%   WTree is a worker-tree produces by wtree:create.
+%   DataKey is the name of a list of vectors that is distributed across the workers.
+%   Here's an example of how one could use this:
+%       W = wtree:create(8).  % create a tree of eight worker processes
+%       % create a list
+%       VList = [hw2:vec_add(N, [1,2,3]) || N <- lists:seq(0,1000)], ok.
+%       % divide VList and send the pieces to the workers
+%       workers:update(W, vdata, misc:cut(VList, W)).
+%       VMean = hw2:vec_mean(W, vdata).
+%   The last command should set VMean to [501.0, 502.0, 503.0]
+vec_mean(_WTree, _DataKey) -> 0.0.  % this is a stub
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% I'm providing vec_add and vec_sum   %
+% to make your life easier.           %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% vec_add(X1, X2) -> X1 + X2
+%   X1 and X2 can be vectors of the same length, or one or both of X1 or X2
+%   may be plain numbers.  A vector is a list of numbers.
+%   If both X1 and X2 are vectors, we return the vector sum:
+%     vec_add([1,2,3], [1,4,9]) -> [2,6,12]
+%   If X1 is a scalar and X2 is a vector, we add X1 to each element of X2.
+%   Likewise if X1 is a vector and X2 is a scalar:
+%     vec_add(1, [1,4,9]) -> [2,5,10]
+%     vec_add([1,2,3], 2) -> [3,4,5]
+%   If both X1 and X2 are scalars, then we just return their sum:
+%     vec_add(1, 2) -> 3
+vec_add(S1, S2) when is_number(S1), is_number(S2) -> S1 + S2;
+vec_add(S1, V2) when is_number(S1), is_list(V2) -> [ S1 + S2 || S2 <- V2];
+vec_add(V1, S2) when is_list(V1), is_number(S2) -> [ S1 + S2 || S1 <- V1];
+vec_add(V1, V2) when is_list(V1), is_list(V2) ->
+  [ S1 + S2 || {S1, S2} <- lists:zip(V1, V2)].
+
+% vec_sum(VList) -> Sum
+%   Sum is the sum of the elements using vec_add:
+%   vec_sum([[1,2,3], [1,4,9], [1,8,27], 42]) -> [45, 56, 81]
+%     If you try it, the Erlang shell will print [45, 56, 81] as "-8Q".
+vec_sum(VList) -> lists:foldl(fun(E, Acc) -> vec_add(Acc, E) end, 0, VList).
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                          %
+% Q3: set_nth                                                              %
+%                                                                          %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% set_nth(N, Fun, List) -> replace the Nth element of List with Fun(lists:nth(List))
+set_nth(_N, _Fun, _List) -> [].  % this is a stub.
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                          %
+% Q4: bank_statement                                                       %
+%                                                                          %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% bank_statement(WTree, SrcKey, DstKey, InitialBalance)
+%   Compute a bank statement for an account starting with a balance of InitialBalance.
+%   WTree is a worker-tree.  The list of transactions to be performed for the statement
+%     are stored in a list distributed across the workers.
+%   SrcKey is the name of the list of transactions.
+%   DstKey is the name under which to save the list of after-transaction balances.
+bank_statement(_WTree, _SrcKey, _DstKey, _InitialBalance) -> ok.  % this is a stub
+
+% sample_transactions provides the transaction list for the example from
+%   the problem statement.
+sample_transactions() ->  [ % a list of 21 transactions for testing bank_statement
+  % the commented columns correspond to the balance after each transaction with an
+  %  initial balance of    0.00      1000.00
+  {deposit,  100.00},  % 100.00      1100.00
+  {deposit,  500.00},  % 600.00      1600.00
+  {withdraw,  50.00},  % 550.00      1550.00
+  {withdraw,  25.00},  % 525.00      1525.00
+  {withdraw,  14.00},  % 511.00      1511.00
+  {withdraw,  11.00},  % 500.00      1500.00
+  {interest,   5.00},  % 525.00      1575.00
+  {withdraw,  17.00},  % 508.00      1558.00
+  {deposit,   42.00},  % 550.00      1600.00
+  {interest,  -3.00},  % 533.50      1552.00
+  {withdraw, 123.45},  % 410.05      1428.55
+  {deposit,   19.95},  % 430.00      1448.50
+  {interest,   2.33},  % 440.02      1482.25
+  {deposit,   -0.33},  % 439.69      1481.92
+  {withdraw, 192.68},  % 247.01      1289.24
+  {withdraw, 300.00},  % -52.99       989.24
+  {interest,  20.00},  % -63.59      1187.09
+  {deposit,   10.00},  % -53.59      1197.09
+  {interest,  -5.00},  % -50.91      1137.23
+  {withdraw,  14.00},  % -64.91      1123.23
+  {deposit,  100.00}   %  35.09      1223.23
+].
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                          %
+% Q5: sliding average                                                      %
+%                                                                          %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+sliding_average(_WTree, _SrcKey, _DstKey, _Kernel, _InitialPrefix) -> ok. % this is a stub
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% The next three functions: dp, sliding_average_help, and        %
+%   sliding_average_seq provide a sequential implementation      %
+%   of sliding_average.  You can use this to get a clearer       %
+%   idea of what sliding_average should do.  In particular:      %
+%     sliding_average_equiv(SrcList, Kernel, InitialPrefix) ->   %
+%       W = wtree:create(),                                      %
+%       workers:update(W, src, misc:cut(SrcList, W)),            %
+%       sliding_average(W, src, dst, Kernel, InitialPrefix),     %
+%       Result = lists:append(workers:retrieve(W, dst)),         %
+%       wtree:reap(W), % clean-up                                %
+%       Result.                                                  %
+%   should return the same thing as sliding_average_help.        %
+%   Of course, you may use any of this code, "as is" or with     %
+%   modifications, in your solution.                             %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% dp(DataVec, KernelVec, Acc) -> DotProduct | done.
+%   This is a variant on dotproduct.
+%     If length(DataVec) >= length(KernelVec), then
+%       we compute the dot product of KernelVec with the first
+%       length(KernelVec) elements of DataVec.
+%     Otherwise (DataVec is shorter than KernelVec)
+%       we return 'done'.
+dp(_Data, [], Acc) -> Acc;
+dp([], _Kernel, _Acc) -> done;
+dp([HData | TData], [HKernel | TKernel], Acc) ->
+  dp(TData, TKernel, Acc + HData*HKernel).
+
+% sliding_average_help(Data, Kernel, Acc) -> AvgData
+%   Slide the Kernel along Data, computing the weighted averages as we go.
+%   When Kernel is longer than the remaining Data, we return.
+%     We want AvgData to be in the same order as Data, but we also want to
+%   be able to handle large lists for Data.  So, the implementation is
+%   tail-recursive with a call to lists:reverse at the end.  This means
+%   we make two tail-recursive passes, but we avoid creating millions of
+%   stack frames.
+sliding_average_help(Data, Kernel, Acc) ->
+  case dp(Data, Kernel, 0.0) of
+    done -> lists:reverse(Acc);
+    X -> sliding_average_help(tl(Data), Kernel, [X | Acc])
+  end.
+
+% sliding_average_seq(SrcList, Kernel, InitialPrefix)
+%   A sequential implementation of sliding_average.
+sliding_average_seq(SrcList, Kernel, InitialPrefix)
+    when is_list(Kernel), is_list(InitialPrefix),
+         length(InitialPrefix) =:= length(Kernel)-1 ->
+  ExtendedSrc = InitialPrefix ++ SrcList,
+  sliding_average_help(ExtendedSrc, Kernel, []).
+
+% avg_demo() performs the computation described in the Figure for the
+%   sliding window question and returns the result.
+avg_demo() ->
+  Kernel =  [0.25, 0.25, 0.5],
+  SrcList = [1, 2, 10, 12, 14, 2, 2, 2, 0, -1, -8, 14],
+  InitialPrefix = [0, 0],
+  DstList = sliding_average_seq(SrcList, Kernel, InitialPrefix),
+  DstList.
---
+++ hw2/hw2_test.erl
@@ -0,0 +1,309 @@
+-module(hw2_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-import(hw2, [mean/2, vec_mean/2, set_nth/3, bank_statement/4, sliding_average/5]).
+-export([vec_close/2, sliding_average_expected/4, sliding_average_run/5]).
+
+-define(errTol, 1.0e-12).  % round-off error tolerance for floating point results
+
+% NOTE:  To write these tests, I had to learn to use EUnit test fixtures.
+% If you like, you can read all about them in
+%     http://learnyousomeerlang.com/eunit#fixtures
+% Or, you can just use my code.  The test fixtures provid set-up and
+% tear-down code for the test.  I use these to create the process trees
+% and terminate the worker processes at the end of a batch of tests.
+% I also usd the init functions to set-up test data lists in the ProcState
+% maps.
+%   You can use all this.  For each function required in the homework,
+% you will find two functions that you can modify to add more tests:
+%   function_name_init() -> WorkerTree
+%     You can see where I did a workers:update(W, Key, misc:cut(List, W))
+%     one or more times in each of these.  This is where I'm setting up
+%     data in the ProcState maps to be used in the tests.  You can add
+%     data for your test cases here.  Just give each list (or other data
+%     value) that you need a different key.
+%   function_name_test_cases(W) -> ListofTestCases
+%     This function returns a list of tests.  You can add more
+%       ?_assert(...)
+%       ?_assertEqual(...)
+%       ?_assertError(...)
+%       ...
+%   tests to the list.  Note the '_' in front of 'assert' in these macro
+%   names.  This creates a tuple that describes the test to be run.  The
+%   I don't think the details are essential -- it's just part of the
+%   set-up, run-tests, clean-up pattern.  For writing tests, you just need
+%   to remember to put that '_' in front of the name of the assert macro.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                          %
+%  Q1: mean(WTree, DataKey)                                                %
+%                                                                          %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+mean_init() ->
+  W = wtree:create(8),
+  % The example from the template file.
+  workers:update(W, data, misc:cut(lists:seq(1,1000), W)),
+
+  % by using a list of integers and making the total number of elements
+  % in the list a power of two, we shouldn't have to deal with roundoff error
+  wtree:rlist(W, 64, 42, random_ints),
+
+  % OK, I'll deal with the round-off to show how it can be done.
+  wtree:rlist(W, 42, 1.0, random_floats),
+
+  % If our list is shorter than W, then some of the workers will have
+  %   empty lists.  mean(W, Key) should compute the correct answer anyway.
+  workers:update(W, shortlist, misc:cut(lists:seq(1, (2*length(W)) div 3), W)),
+
+  % mean should fail for an empty list.
+  workers:update(W, emptylist, misc:cut([], W)),
+
+  % You should add at least one more test case: create the data here
+  % and test it in the mean_test_cases(W) function below.
+
+  W. % return the worker tree
+% end mean_init
+
+mean_test_cases(W) ->
+  Mean = fun(List) -> lists:sum(List) / length(List) end, % reference function
+  [ ?_assertEqual(Mean(lists:append(workers:retrieve(W, data))),
+                  mean(W, data)),
+    ?_assertEqual(Mean(lists:append(workers:retrieve(W, random_ints))),
+                  mean(W, random_ints)),
+    ?_assert(    abs(   Mean(lists:append(workers:retrieve(W, random_floats)))
+                      - mean(W, random_floats))
+	      =< errTol),
+    ?_assert(    abs(   Mean(lists:append(workers:retrieve(W, shortlist)))
+                      - mean(W, shortlist))
+	      =< errTol),
+    ?_assertError(_, mean(W, emptylist))
+    % You should add at least one more test case: create the in the
+    % mean_init() function above and test it here.
+  ].
+
+mean_test_() ->
+  {setup, fun() -> mean_init() end,         % set up
+          fun(W) -> wtree:reap(W) end,      % clean up
+          fun(W) -> mean_test_cases(W) end  % the test cases
+  }.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                          %
+%  Q2: vec_mean(WTree, DataKey)                                            %
+%                                                                          %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+vec_mean_init() ->
+  W = wtree:create(42),
+  % The example from the template file.
+  VList = [vec_add(N, [1,2,3]) || N <- lists:seq(0,1000)],
+  workers:update(W, vdata, misc:cut(VList, W)),
+
+  % You should add more test cases.  The examples from mean_init() and
+  % mean_test_cases() should provide "inspiration".
+  W.
+
+% vec_add (V1, V2)
+%   vec_add was provided in the template for hw2.erl.  There was nothing
+%   in the assignment that said that it has to remain in the solution.
+%   I'm creating a copy in this module so I know that I'll get the
+%   function that I intended.
+vec_add(S1, S2) when is_number(S1), is_number(S2) -> S1 + S2;
+vec_add(S1, V2) when is_number(S1), is_list(V2) -> [ S1 + S2 || S2 <- V2];
+vec_add(V1, S2) when is_list(V1), is_number(S2) -> [ S1 + S2 || S1 <- V1];
+vec_add(V1, V2) when is_list(V1), is_list(V2) ->
+  [ S1 + S2 || {S1, S2} <- lists:zip(V1, V2)].
+
+% vec_sum(VList) -> Sum
+%   vec_sum was provided in the template for hw2.erl.  Again, I'm including
+%   a copy here to make sure the test cases have the function I want.
+vec_sum(VList) -> lists:foldl(fun(E, Acc) -> vec_add(Acc, E) end, 0, VList).
+
+% check if two vectors are "close"
+%   If all components are the same to within ?errTol, we return true
+%   Otherwise, we return a list where each element is 'ok' if the corresponding
+%     elements of V1 and V2 are close enough, and {X1,X2} otherwise, where X1
+%     is the element of V1, and x2 is the element of V2.
+vec_close(V1, V2) ->
+  case length(V1) == length(V2) of
+    true ->
+      case lists:all(fun({X1,X2}) -> abs(X1-X2) =< ?errTol end, lists:zip(V1, V2)) of
+	true -> true;
+	_ -> [    case abs(X1-X2) =< ?errTol*min(abs(X1), 1) of
+		    true  -> ok;
+		    false -> {X1,X2}
+		  end
+	       || {X1, X2} <- lists:zip(V1, V2)
+	     ]
+      end;
+    false -> mismatched_vector_lengths
+  end.
+
+vec_mean_test_cases(W) ->
+  VecMean = fun(List) ->
+    N = length(List),
+    [ X/N || X <- vec_sum(List) ]
+  end,
+  [ ?_assert(vec_close(VecMean(lists:append(workers:retrieve(W, vdata))),
+                      vec_mean(W, vdata)))
+    % You should add more test cases here.
+  ].
+
+vec_mean_test_() ->
+  {setup, fun() -> vec_mean_init() end,         % set up
+          fun(W) -> wtree:reap(W) end,          % clean up
+          fun(W) -> vec_mean_test_cases(W) end  % the test cases
+  }.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                          %
+%  Q3: set_nth(N, Fun, List)                                               %
+%                                                                          %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+set_nth_test_() ->
+  L = [1,4,9,16],
+  F = fun(X) -> X*X + 2 end,
+  [ ?_assertEqual([1,4,83,16], set_nth(3, F, L)),
+    ?_assertError(_, set_nth(5, F, L))
+    % you should add more tests here
+  ].
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                                 %
+%  Q4: bank_statement(WTree, SrcKey, DstKey, InitialBalance)set_nth(N, Fun, List) %
+%                                                                                 %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+bank_statement_init() ->
+  W = wtree:create(8),
+  Transactions = [ % example from the hw2.erl template
+    {deposit,  100.00},
+    {deposit,  500.00},
+    {withdraw,  50.00},
+    {withdraw,  25.00},
+    {withdraw,  14.00},
+    {withdraw,  11.00},
+    {interest,   5.00},
+    {withdraw,  17.00},
+    {deposit,   42.00},
+    {interest,  -3.00},
+    {withdraw, 123.45},
+    {deposit,   19.95},
+    {interest,   2.33},
+    {deposit,   -0.33},
+    {withdraw, 192.68},
+    {withdraw, 300.00},
+    {interest,  20.00},
+    {deposit,   10.00},
+    {interest,  -5.00},
+    {withdraw,  14.00},
+    {deposit,  100.00}
+  ],
+  workers:update(W, transactions, misc:cut(Transactions, W)),
+  W.
+
+bank_statement_run(W, SrcKey, DstKey, InitialBalance) ->
+  bank_statement(W, SrcKey, DstKey, InitialBalance),
+  lists:append(workers:retrieve(W, DstKey)).
+
+bank_statement_test_cases(W) ->
+  [ ?_assert(vec_close(
+      [ 200.00, 700.00, 650.0, 625.0, 611.0, 600.0, 630.0, 613.0, 655.0,
+        635.35, 511.90, 531.85, 544.242105, 543.912105, 351.232105,
+        51.232105, 61.478526, 71.478526, 67.9045997, 53.9045997, 153.9045997
+      ],
+      bank_statement_run(W, transactions, statement, 100.0)
+    ))
+    % You should add more test cases here
+  ].
+  
+bank_statement_test_() ->
+  {setup, fun() -> bank_statement_init() end,         % set up
+          fun(W) -> wtree:reap(W) end,                % clean up
+          fun(W) -> bank_statement_test_cases(W) end  % the test cases
+  }.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                          %
+% Q5: sliding average                                                      %
+%                                                                          %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+sliding_average_init() ->
+  W = wtree:create(8),
+  % The example from the template file.
+  Data = [ I rem 5 || I <- lists:seq(1, 57) ],
+  workers:update(W, data, misc:cut(Data, W)),
+  W.
+
+sliding_average_test_cases(W) ->
+  Kernel1 = [0.125, 0.25, 0.625],
+  InitialPrefix1 = [0, -20],
+  [ ?_assert(vec_close(
+      sliding_average_expected(W, data, Kernel1, InitialPrefix1),
+      sliding_average_run(W, data, data_slide, Kernel1, InitialPrefix1)))
+    % You should add more test cases here
+  ].
+
+sliding_average_test_() ->
+  {setup, fun()  -> sliding_average_init() end,        % set up
+          fun(W) -> wtree:reap(W) end,                 % clean up
+          fun(W) -> sliding_average_test_cases(W) end  % the test cases
+  }.
+
+sliding_average_expected(W, SrcKey, Kernel, InitialPrefix) ->
+  sliding_average_seq(lists:append(workers:retrieve(W, SrcKey)),
+                      Kernel, InitialPrefix).
+
+sliding_average_run(W, SrcKey, DstKey, Kernel, InitialPrefix) ->
+  sliding_average(W, SrcKey, DstKey, Kernel, InitialPrefix),
+  lists:append(workers:retrieve(W, DstKey)).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% The next four functions: dp, sliding_average_help, sliding_average_seq, %
+%   and sliding_average_equiv provide a sequential implementation of      %
+%   sliding_average.  I got these from the template file for hw2.erl.     %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% dp(DataVec, KernelVec, Acc) -> DotProduct | done.
+%   This is a variant on dotproduct.
+%     If length(DataVec) >= length(KernelVec), then
+%       we compute the dot product of KernelVec with the first
+%       length(KernelVec) elements of DataVec.
+%     Otherwise (DataVec is shorter than KernelVec)
+%       we return 'done'.
+dp(_Data, [], Acc) -> Acc;
+dp([], _Kernel, _Acc) -> done;
+dp([HData | TData], [HKernel | TKernel], Acc) ->
+  dp(TData, TKernel, Acc + HData*HKernel).
+
+% sliding_average_help(Data, Kernel, Acc) -> AvgData
+%   Slide the Kernel along Data, computing the weighted averages as we go.
+%   When Kernel is longer than the remaining Data, we return.
+%     We want AvgData to be in the same order as Data, but we also want to
+%   be able to handle large lists for Data.  So, the implementation is
+%   tail-recursive with a call to lists:reverse at the end.  This means
+%   we make two tail-recursive passes, but we avoid creating millions of
+%   stack frames.
+sliding_average_help(Data, Kernel, Acc) ->
+  case dp(Data, Kernel, 0.0) of
+    done -> lists:reverse(Acc);
+    X -> sliding_average_help(tl(Data), Kernel, [X | Acc])
+  end.
+
+% sliding_average_seq(SrcList, Kernel, InitialPrefix)
+%   A sequential implementation of sliding_average.
+sliding_average_seq(SrcList, Kernel, InitialPrefix)
+    when is_list(Kernel), is_list(InitialPrefix),
+         length(InitialPrefix) =:= length(Kernel)-1 ->
+  ExtendedSrc = InitialPrefix ++ SrcList,
+  sliding_average_help(ExtendedSrc, Kernel, []).
